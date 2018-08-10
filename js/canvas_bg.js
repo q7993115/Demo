3d2f3f4e82eb60d9864d5341a410e5504349a73e
Zain <297486678@qq.com>
2018-08-10 15:40:08 +0800

    8.10

--- js/canvas_bg.js
+++ js/canvas_bg.js
@@ -5,12 +5,12 @@
 			var w,h;
 			var pi = Math.PI;
 			var all_attribute = {
-				num:60,            			 // 个数
+				num:30,            			 // 个数
 				start_probability:0.1,		     // 如果数量小于num，有这些几率添加一个新的     		     
 				size_min:1,   			     // 初始爱心大小的最小值
 				size_max:2,   			     // 初始爱心大小的最大值
-				size_add_min:0.1,               // 每次变大的最小值（就是速度）
-				size_add_max:0.2,               // 每次变大的最大值
+				size_add_min:0.08,               // 每次变大的最小值（就是速度）
+				size_add_max:0.15,               // 每次变大的最大值
 				opacity_min:0.3,                 // 初始透明度最小值
 				opacity_max:0.5, 				 // 初始透明度最大值
 				opacity_prev_min:.003,            // 透明度递减值最小值
@@ -24,7 +24,7 @@
 			function start(){
 				window.requestAnimationFrame(start);
 				style_color+=0.1;
-				//更改背景色hsl(颜色值，饱和度，明度)
+				//更改背景色hsl(颜色值，饱和度，亮度)
 				ctx.fillStyle = 'hsl('+style_color+',100%,97%)';
 				ctx.fillRect(0, 0, w, h);
 				if (all_element.length < all_attribute.num && Math.random() < all_attribute.start_probability){
